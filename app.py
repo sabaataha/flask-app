601df9141057034b7ac9317deaba5201118cf4a8
sabaataha <sabaataha@gmail.com>
2024-07-12 00:04:00 +0300

    starting flask app

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask , request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/ask" ,  methods=['POST'])
+def ask_question():
+    data = request.get_json()
+    question_text = data.get('question')
+    
+    if not question_text:
+        return jsonify({'error': 'No question provided'}), 400
+    
+    return jsonify({'question': question_text}), 200
+  
+
+if __name__ == '__main__':
+    app.run(debug=True)
